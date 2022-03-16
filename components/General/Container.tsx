37f365fb017c01b72ebfda69c6e08a68aab3eadd
Ralf <ralflopez3@gmail.com>
2022-03-16 22:46:38 +0800

    :art: navbar styling

---
+++ components/General/Container.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Container = ({ children }: any) => {
+  return (
+    <div className='flex justify-center w-full pt-20'>
+      <div className='w-11/12 md:w-10/12 max-w-7xl'>{children}</div>
+    </div>
+  )
+}
+
+export default Container
