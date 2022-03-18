dccbbc818cb708813f411b178a8e33fff2f38ed8
Ralf <ralflopez3@gmail.com>
2022-03-18 22:58:26 +0800

    :lipstick: style buy modal for individual assets

--- components/General/Modal.tsx
+++ components/General/Modal.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { X } from "react-bootstrap-icons"
 
 interface Props {
   title: string
@@ -14,13 +15,18 @@ const Modal = ({ title, toggle, children }: Props) => {
   return (
     <>
       <div
-        className='absolute top-0 left-0 z-10 w-full h-full bg-slate-800 opacity-40'
+        className='absolute top-0 left-0 z-40 w-full h-full bg-slate-800 opacity-40 backdrop-blur-3xl'
         onClick={handleToggle}
       ></div>
-      <div className='absolute z-20 -translate-x-1/2 bg-red-700 left-1/2 top-5'>
-        <div className='flex justify-between px-3 py-2 w-96'>
-          <h5>{title ? title : " "}</h5>
-          <div onClick={handleToggle}>Close</div>
+      <div className='absolute z-50 pb-4 -translate-x-1/2 bg-gray-200 rounded-sm shadow-sm left-1/2 top-8'>
+        <div className='flex items-center justify-between px-3 py-2 bg-white w-96'>
+          <h5 className='font-bold'>{title ? title : " "}</h5>
+          <div
+            onClick={handleToggle}
+            className='flex items-center transition-colors duration-300 ease-out rounded-full cursor-pointer hover:bg-gray-300'
+          >
+            <X width={30} height={30} />
+          </div>
         </div>
         <div className='p-3'>{children}</div>
       </div>
