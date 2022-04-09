ad4023d9de5b80142cc534a91008c962b2d50a48
Ralf <ralflopez3@gmail.com>
2022-04-09 23:19:31 +0800

    :lipstick change continer padding top to add more space

--- components/General/Container.tsx
+++ components/General/Container.tsx
@@ -1,8 +1,13 @@
 import React from "react"
 
-const Container = ({ children }: any) => {
+interface Props {
+  children: any
+  className?: string
+}
+
+const Container = ({ children, className }: Props) => {
   return (
-    <div className='flex justify-center w-full pt-20 pb-8'>
+    <div className={`flex justify-center w-full pt-20 pb-8 ${className}`}>
       <div className='w-11/12 md:w-10/12 max-w-7xl'>{children}</div>
     </div>
   )
